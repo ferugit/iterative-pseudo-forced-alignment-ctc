89b36599b574985a69e03d36905019bf5c6f2d1d
ferugit <wiliam.lopezgavilanez@telefonica.com>
2023-01-05 13:09:02 +0100

    fix post-processor to add collar

--- src/postprocess/postprocess_and_filter.py
+++ src/postprocess/postprocess_and_filter.py
@@ -1,7 +1,40 @@
 import os
 import argparse
-
 import pandas as pd
+
+import torchaudio
+
+
+def apply_start_collar(x, delta):
+    if (x - delta) > 0.0:
+        return x - delta
+    else:
+        return 0.0
+
+def apply_end_collar(df, delta):
+    old_audio_path = None
+    list_of_end_times = []
+
+    for _, row in df.iterrows():
+        audio_path = row['Sample_Path']
+
+        if old_audio_path != audio_path:
+            info = torchaudio.info(audio_path)
+            audio_length_time = info.num_frames/info.sample_rate
+        
+        end = float(row['End'])
+
+        if (end + delta) > audio_length_time:
+            new_end = audio_length_time
+        else:
+            new_end = end + delta
+
+        old_audio_path = audio_path
+        list_of_end_times.append(new_end)
+
+    return pd.Series(list_of_end_times)
+
+    
 
 
 def main(args):
@@ -19,10 +52,14 @@ def main(args):
         filtered_df['Start'] += args.right_offset
         filtered_df['End'] += args.left_offset
         filtered_df['Audio_Length'] = filtered_df['End'] - filtered_df['Start']
-        # REMOVE: this code will not be needed in the future
-        filtered_df['Sample_ID'] = filtered_df['Sample_ID'].apply(lambda x: x.split('/')[-1].replace('.wav', ''))
-        
         audio_seconds = filtered_df['Audio_Length'].sum()
+
+        # apply collar
+        if args.collar > 0.0:
+            delta = args.collar/2
+            filtered_df['Start'] = filtered_df['Start'].apply(lambda x: apply_start_collar(x, delta))
+            filtered_df['End'] = apply_end_collar(filtered_df, delta)
+
         print('Total audio length {0} seconds'.format(audio_seconds))
 
         filtered_df.to_csv(os.path.join(os.path.dirname(args.tsv), partition_name + '_filtered.tsv'), index=None, sep='\t')
@@ -43,11 +80,11 @@ if __name__ == "__main__":
     parser.add_argument('--minimum_score', type=float, default=-1.0, metavar='TH', help='minimum threshold')
 
     # Deviation
-    parser.add_argument('--offset_time', type=float, default=0.0, metavar='offset', help='time window covered by every data sample')
-    parser.add_argument("--left_offset", type=float, default=0.0, metavar='left_offset', help='left offset to correct deviation')
-    parser.add_argument("--right_offset", type=float, default=0.0, metavar='right_offset', help='right offset to correct deviation')
+    parser.add_argument('--offset_time', type=float, default=0.0, metavar='offset', help='temporal shift in seconds of alignment (left and rigth)')
+    parser.add_argument("--left_offset", type=float, default=0.0, metavar='left_offset', help='left offset in seconds')
+    parser.add_argument("--right_offset", type=float, default=0.0, metavar='right_offset', help='right offset in seconds')
+    parser.add_argument('--collar', type=float, default=0.0, metavar='collar', help='collar to apply to alignment in seconds')
 
-    # TODO: add collar arg
     args = parser.parse_args()
 
     # Run main
