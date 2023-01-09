16755db5af6dbb661997a15b751b705e7ddd286e
ferugit <wiliam.lopezgavilanez@telefonica.com>
2023-01-09 17:19:27 +0100

    remove unneeded comments from generate scripts

--- generate_clips.sh
+++ generate_clips.sh
@@ -5,10 +5,8 @@
 # This script generates clips from aligned data
 # Used columns: Sample_Path, Sample_ID, Start, End
 
-#src=data/wip_benedetti/results/benedetti_aligned_filtered.tsv
-#dst=data/wip_benedetti/clips/
-
-src=data/wip_benedetti_words/results/filtered_aligned.tsv
-dst=data/wip_benedetti_words/clips
+src=data/wip_benedetti/results/benedetti_aligned_filtered.tsv
+dst=data/wip_benedetti/clips/
+mkdir -p $dst
 
 python -u src/scripts/generate_clips.py --src $src --dst $dst
