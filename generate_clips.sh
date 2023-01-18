f77e37a6f6dbc801f5b023f4350d6876c397d9c8
ferugit <wiliam.lopezgavilanez@telefonica.com>
2023-01-18 16:45:14 +0100

    correct clip generation to be used with the sample data

--- generate_clips.sh
+++ generate_clips.sh
@@ -5,8 +5,8 @@
 # This script generates clips from aligned data
 # Used columns: Sample_Path, Sample_ID, Start, End
 
-src=data/wip_benedetti_words_2/results/benedetti_aligned_words.tsv
-dst=data/wip_benedetti_words_2/clips/
+src=data/wip_benedetti_words/results/benedetti_aligned_words.tsv
+dst=data/wip_benedetti_words/clips/
 mkdir -p $dst
 
 python -u src/scripts/generate_clips.py --src $src --dst $dst
